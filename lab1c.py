7252f9ae96b67d0a09d811af4e1fd9f4f6061fdf
Tahsin Absar <mtabsar@myseneca.ca>
2026-01-13 07:42:56 -0500

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Description: This program will output "Isaac is 72 years old!" to the screen.'''
+
+name = 'Isaac'
+age = 72
+
+print(name + ' is ' + str(age) + ' years old!')
